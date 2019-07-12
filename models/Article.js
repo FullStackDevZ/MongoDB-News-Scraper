72fdf33325eaa6d8e31c9bd57e97c047abd07ec8
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-07-11 20:37:33 -0700

    updated

--- models/Article.js
+++ models/Article.js
@@ -3,6 +3,42 @@ var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
+// Create the headlineSchema with our schema class
+var ArticleSchema = new Schema({
+  // headline, a string, must be entered
+  headline: {
+    type: String,
+    required: true,
+    unique: { index: { unique: true } }
+  },
+  // summary, a string, must be entered
+  summary: {
+    type: String,
+    required: true
+  },
+  // url, a string, must be entered
+  url: {
+    type: String,
+    required: true
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// Create the Headline model using the headlineSchema
+var Headline = mongoose.model("Headline", ArticleSchema);
+
+// Export the Headline model
+module.exports = Headline;
+
+
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
--- models/Note.js
+++ models/Note.js
@@ -5,11 +5,20 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new NoteSchema object
 // This is similar to a Sequelize model
 var NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
-  body: String
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // as is the noteText
+  noteText: String
 });
+
 
 // This creates our model from the above schema, using mongoose's model method
 var Note = mongoose.model("Note", NoteSchema);
